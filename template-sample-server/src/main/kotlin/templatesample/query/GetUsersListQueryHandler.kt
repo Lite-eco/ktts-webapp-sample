8e7f03c36f5e2cabb758480cbd5dd4a00ef56a2e
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-08-03 18:09:28 +0200

    [ktts-webapp-template] Admin users list table

---
+++ template-sample-server/src/main/kotlin/templatesample/query/GetUsersListQueryHandler.kt
@@ -0,0 +1,16 @@
+package templatesample.query
+
+import templatesample.domain.UserInfos
+import templatesample.repository.user.UserDao
+import org.springframework.stereotype.Service
+
+@Service
+class GetUsersListQueryHandler(val userDao: UserDao) :
+    QueryHandler.Handler<GetUsersListQuery, GetUsersListQueryResponse>() {
+
+    override fun handle(query: GetUsersListQuery) =
+        GetUsersListQueryResponse(
+            userDao.streamAll().toList().map {
+                UserInfos(it.id, it.mail, it.displayName, it.roles)
+            })
+}
