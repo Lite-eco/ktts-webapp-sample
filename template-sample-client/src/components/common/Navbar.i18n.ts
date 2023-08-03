0712e53dd217d96d11eb5d5422b71b95232f4688
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-08-03 18:30:54 +0200

    [ktts-webapp-template] A temporary proposition to prepare i18n

---
+++ template-sample-client/src/components/common/Navbar.i18n.ts
@@ -0,0 +1,10 @@
+import { i18n } from '../../i18n';
+
+export const t = i18n({
+  TemplateSample: () => 'Template sample',
+  Login: () => 'Login',
+  Register: () => 'Register',
+  Account: () => 'Account',
+  UsersManagement: () => 'Users management',
+  ManualCommands: () => 'Manual commands'
+});
