9a24dbd778a00ce00aa2b05d58d3f691858cafcd
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-09-10 00:13:58 +0200

    [ktts-webapp-template] SQL preparing mail update + mail log keeps "all"

--- template-sample-server/src/main/resources/data/schema/log/user_mail_log.sql
+++ template-sample-server/src/main/resources/data/schema/log/user_mail_log.sql
@@ -3,7 +3,8 @@ CREATE TABLE user_mail_log
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
     mail VARCHAR(255) NOT NULL,
-    type VARCHAR(255) NOT NULL,
+    dirty_mail VARCHAR(255),
+    validated BOOLEAN NOT NULL,
     creation_date TIMESTAMPTZ NOT NULL,
     FOREIGN KEY (user_id) REFERENCES app_user (id)
 );
--- template-sample-server/src/main/resources/data/schema/user/user_account_operation_token.sql
+++ template-sample-server/src/main/resources/data/schema/user/user_account_operation_token.sql
@@ -3,6 +3,8 @@ CREATE TABLE user_account_operation_token
     token VARCHAR(40) PRIMARY KEY,
     token_type VARCHAR(255) NOT NULL,
     user_id UUID NOT NULL,
+    user_mail_log_id UUID,
     creation_date TIMESTAMPTZ NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES app_user (id)
+    FOREIGN KEY (user_id) REFERENCES app_user (id),
+    FOREIGN KEY (user_mail_log_id) REFERENCES user_mail_log (id)
 );
